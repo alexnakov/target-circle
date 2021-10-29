cde40162f0280a26cc94154c85452844d4496ec3
Alex Nakov <alex.nakov3@gmail.com>
2021-10-29 13:53:58 +0100

    onclick: startbutton dissapears and game div appears

---
+++ script.js
@@ -0,0 +1,7 @@
+const gameScreen = document.getElementById('gameScreen')
+const startGameButton = document.getElementById('startGameButton')
+
+function startGame() { // startGameButton function
+  startGameButton.style.display = "none"
+  gameScreen.style.display = 'flex'
+}
